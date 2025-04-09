5fb7891f21720561b662ab8e39e9b1c29deda061
timkiemnhantai <khangnhgps40064@gmail.com>
2025-04-09 16:38:23 +0700

    check

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Calculator {
+    public Integer sum(Integer a, Integer b) {
+        return 0;
+    }
+
+    public Integer sub(Integer a, Integer b) {
+        return 0;
+    }
+}
